165d6b10e460fd1bb0982d41ead8c392098aa1e8
chkrishnamurthy <chalapatikrishna@gmail.com>
2020-12-08 21:47:14 +0530

    changes done for final

--- scripts/eventPage.js
+++ scripts/eventPage.js
@@ -16,38 +16,17 @@ chrome.contextMenus.onClicked.addListener((clickData) => {
       chrome.storage.sync.set({ text: a });
     });
 
-    const g = {
-      url: chrome.extension.getURL("popup.html"),
-      type: "popup",
-      top: 100,
-      left: 50,
-      width: 730,
-      height: 575,
-      focused: true,
-      state: "normal",
-    };
-    chrome.windows.create(g);
+    // const g = {
+    //   url: chrome.extension.getURL("popup.html"),
+    //   type: "popup",
+    //   top: 100,
+    //   left: 50,
+    //   width: 730,
+    //   height: 575,
+    //   focused: true,
+    //   state: "normal",
+    // };
+    // chrome.windows.create(g);
     // chrome.windows.create(createData);
   }
 });
-
-// chrome.runtime.onMessage.addListener(
-//     function(request, sender, sendResponse) {
-//         console.log(request.greeting);
-//         console.log(sender);
-//         console.log(sendResponse);
-//         // if (request.greeting == "hello")
-//         // sendResponse({farewell: "goodbye"});
-
-//     });
-
-// function onCreated(window) {
-//   console.log("Created");
-//   chrome.windows.update(
-//     window.id,
-//     { state: "maximized" },
-//     function onUpdated() {
-//       console.log("Maximized");
-//     }
-//   );
-// }
